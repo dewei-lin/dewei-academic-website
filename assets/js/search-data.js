cba2dfe14c92286782b10e5f4e721ee056b92a45
dewei-lin <lindewei0423@gmail.com>
2026-05-03 20:10:58 +0000

    Deploying to gh-pages from @ dewei-lin/dewei-academic-website@edb4d90c110c0431256533cb6796d7863bc1818f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-",
-    title: "",
+    id: "nav-about",
+    title: "about",
     section: "Navigation",
     handler: () => {
       window.location.href = "/dewei-academic-website/";
@@ -466,24 +466,10 @@ ninja.data = [{
         },
       },{
         id: 'social-github',
-        title: 'Github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("", "_blank");
-        },
-      },{
-        id: 'social-linkedin',
-        title: 'Linkedin',
-        section: 'Socials',
-        handler: () => {
-          window.open("", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=", "_blank");
+          window.open("https://github.com/dewei-lin", "_blank");
         },
       },{
         id: 'social-rss',
