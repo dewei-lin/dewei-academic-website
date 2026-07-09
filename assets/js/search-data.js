a1b8abcb2c1aa4888a88bdcbb70c02fa10bf2a31
dewei-lin <lindewei0423@gmail.com>
2026-07-09 19:55:39 +0000

    Deploying to gh-pages from @ dewei-lin/dewei-academic-website@8ad52c558706eb4b4a86acf926ca889f11605ed6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "My CV uses the template from Awesome CV.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/dewei-academic-website/cv/";
